c60076104bdb07954d04b5d48bea29f5ca959c28
gossi <github@gos.si>
2012-06-20 11:12:16 +0200

    Initial commit

---
+++ babel.js
@@ -0,0 +1,58 @@
+var babel = (function (window, document) {
+
+	var config = {
+		lang: "en",
+		dir: "lang",
+		extension: "json"
+	};
+
+	var next;
+	var loaded = {};
+
+	var loadLanguage = function(lang) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", config.dir + "/" + lang + "." + config.extension, true);
+		xhr.addEventListener("load", function(e) {
+			loaded[lang] = JSON.parse(xhr.responseText);
+			
+			if (next == lang) {
+				replace(lang);
+			}
+		}, false);
+		xhr.addEventListener("error", function(e) {
+			loaded[lang] = {};
+		}, false);
+		xhr.send();
+	};
+	
+	var replace = function(lang) {
+		Object.keys(loaded[lang]).forEach(function (key) {
+			document.getElementById(key).innerHTML = loaded[lang][key]; 
+		});
+	};
+	
+	var babel = {
+		init : function(options) {
+			Object.keys(options).forEach(function (key) {
+				config[key] = options[key];
+			});
+		},
+		
+		activate: function(lang) {
+			if (!loaded[lang]) {
+				loadLanguage(lang);
+				next = lang;
+			} else {
+				replace(lang);
+			}
+		}
+	};
+	
+	// bootstrap
+	document.addEventListener("DOMContentLoaded", function (e) {
+		babel.activate(config.lang);
+	});
+	
+	return babel;
+	
+})(window, document);
